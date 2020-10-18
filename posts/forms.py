22e842e0fe43998352e19bb9098aa2976207a31d
dyachkov-pavel <pahanthebest12345@mail.ru>
2020-10-18 15:51:02 +0300

    commented image in forms

--- posts/forms.py
+++ posts/forms.py
@@ -6,6 +6,6 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['group', 'text', 'image'] # +image
+        fields = ['group', 'text',] #'image'
         labels = {'group':'Выберите группу', 'text':'Введите текст','image':'Загрузите картинку'}
         help_texts = {'group':'Из уже существующих', 'text':'Любой текст', 'image':'Любой файл'}
